2989d7a5e289fff9185ec74e1c4fb9bf8748ab0b
henry <hnr.leto11@gmail.com>
2020-08-31 08:25:21 -0400

    Added additional tesdting

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,71 @@
 # frozen_string_literal: true
 
-require 'board.rb'
+require './board'
 
 # holds player choices and game decisions
 class Game
-  def initialize(player1, player2)
+  attr_reader :player1, :player2, :p1symbol, :p2symbol
+  def initialize
     @player1 = player1
+    @p1symbol = p1symbol
+    @p2symbol = p2symbol
     @player2 = player2
+    @board = Board.new
+  end
+
+  def intro_text
+    puts <<~HEREDOC
+                  Welcome to Connect-Four!
+        
+          In this game, only the STRONG survive
+        
+      The board is a standard, 6 x 7 Connect Four grid      
+      Players will take turn dropping tokens into a slot,
+      where the game piece fills the last available space.
+        
+      The goal of this game is to WIN. 
+      This can be done by getting 4 game pieces in a row:
+
+      Horizonally, vertically, OR diagonally.
+        
+
+      Keep an eye on your opponent, they may be building off your pieces
+      
+                  for a CONNECT-FOUR
+        
+        
+        
+    HEREDOC
+    p @p1symbol, @p2symbol
+  end
+
+  def start_game
+    intro_text
+  end
+
+  def set_players
+    puts "\nPlayer 1, please enter your name..."
+    @player1 = set_name
+    puts "\n\nPlayer 1, please enter your name..."
+    @player2 = set_name
+  end
+
+  protected
+
+  def set_name
+    user_input = gets.chomp
+    if valid_name?(user_input)
+      puts "Nice to meet you, #{user_input}!"
+    else
+      puts 'Please enter a valid name (a-z)...'
+      set_name
+    end
+  end
+
+  def valid_name?(user_input)
+    user_input.match?(/[a-zA-Z]/)
   end
 end
+
+c4 = Game.new
+c4.intro_text
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,24 @@
 # frozen_string_literal: true
 
 require './lib/game.rb'
+require './lib/board.rb'
 
 describe Game do
-  subject(:game) { described_class.new(player1, player2) }
-  let(:player1) { double('') }
-  context 'at start of game' do
-    it 'initializes player1 and player2' do
-      expect(player1).to be('Henry')
+  subject(:game) { described_class.new }
+  describe '#set_players' do
+    context 'when the game begins' do
+      it 'prompts player 1 and 2 to choose enter names' do
+        # Arrange
+        player1 = 'Henry'
+        allow(game).to receive(:puts)
+        allow(game).to receive(:gets).and_return(player1)
+        allow(game).to receive(:set_name)
+        # Act
+        game.set_players
+        # Assert
+
+        expect(player1).to be('Henry')
+      end
     end
   end
 end
